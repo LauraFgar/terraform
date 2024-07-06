d755e8051142bdf4095c267e1cc4ea4b8f37add7
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2024-07-06 11:58:46 -0500

    feat: add rds config

---
+++ rds.tf
@@ -0,0 +1,52 @@
+# Grupo de Seguridad para RDS
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds_sg"
+  }
+}
+
+# Instancia de RDS
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.default.id
+
+  tags = {
+    Name = "my-rds-instance"
+  }
+}
+
+# Subnet Group para RDS
+resource "aws_db_subnet_group" "default" {
+  name       = "my_db_subnet_group"
+  subnet_ids = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+  tags = {
+    Name = "my_db_subnet_group"
+  }
+}
